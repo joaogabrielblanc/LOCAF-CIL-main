94b6df3af4bd92db6623182c9c19ceaee40ce693
Sasmage <lpopak1234@gmail.com>
2026-06-07 20:19:00 -0300

    reformulaçao da pagina

--- API/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { health } from '../controllers/auth.controller.js';
-
-export const authRouter = express.Router();
-
-authRouter.get('/health',     health.verify);
-authRouter.post('/registrar', health.registrar);
-authRouter.post('/login',     health.login);
-authRouter.get('/perfil',     health.perfil);
--- interface/README.md
+++
@@ -1,1 +0,0 @@
-# LOCAF-CIL
